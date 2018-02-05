c8105ca2d52ac512a49d7ed09c7244e998113421
baldegit <elhibrbalde@gmail.com>
2018-02-05 01:32:10 +0100

    asasasasasasasasasasa

--- monTestGit/src/monTestGit/Main.java
+++ monTestGit/src/monTestGit/Main.java
@@ -4,15 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("mon premier test avec github sur eclipse");
-		System.out.println("je fais ma premier modification");
-		
-		System.out.println("mon push a marche");
-		
-		System.out.println("mon premier test avec github sur eclipse");
-		System.out.println("je fais ma premier modification");
-		
-		System.out.println("mon push a marche");
+				System.out.println("mon premier test avec github sur eclipse");
+				System.out.println("je fais ma premier modification");
+				
+				System.out.println("mon push a marche");
+				
+				System.out.println("mon premier test avec github sur eclipse");
+				System.out.println("je fais ma premier modification");
+				
+				System.out.println("mon push a // TODO Auto-generated method stub\n" + 
+						"		System.out.println(\"mon premier test avec github sur eclipse\");\n" + 
+						"		System.out.println(\"je fais ma premier modification\");\n" + 
+						"		\n" + 
+						"		System.out.println(\"mon push a marche\");\n" + 
+						"		\n" + 
+						"		System.out.println(\"mon premier test avec github sur eclipse\");\n" + 
+						"		System.out.println(\"je fais ma premier modification\");\n" + 
+						"		\n" + 
+						"		System.out.println(\"mon push a marche\");marche");
 		
 	}
 
