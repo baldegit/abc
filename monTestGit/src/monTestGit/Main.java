5f0dbf514656480bf5a5b07c81c31ed71358bdeb
baldegit <elhibrbalde@gmail.com>
2018-02-05 01:00:47 +0100

    ceci est mon peremier commit avec git sur eclipse
    Change-Id: Ib0877361762eb9d25c8c95894c56a4932721cd16
    Signed-off-by: baldegit <elhibrbalde@gmail.com>

---
+++ monTestGit/src/monTestGit/Main.java
@@ -0,0 +1,12 @@
+package monTestGit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("mon premier test avec github sur eclipse");
+		System.out.println("je fais ma premier modification");
+		
+	}
+
+}
